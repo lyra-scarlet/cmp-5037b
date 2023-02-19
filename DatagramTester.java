751442fa1f72ffd3349bb70fb1be3b95330c1db3
lyra-scarlet <118722990+lyra-scarlet@users.noreply.github.com>
2023-02-19 17:43:28 +0000

    Measures average burst gap and length

--- DatagramTester.java
+++ DatagramTester.java
@@ -6,7 +6,6 @@ import java.net.*;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.Random;
-import java.util.HexFormat;
 import uk.ac.uea.cmp.voip.*;
 
 public class DatagramTester {
@@ -66,6 +65,11 @@ public class DatagramTester {
         receiving_socket.setSoTimeout(timeout);
         double loss = 0;
         double accuracy = 0;
+        double burstgap = 1;
+        double burstlength = 0;
+        double avgburstlength = 0;
+        double avgburstgap = 0;
+        int bursts = 0;
         // Main loop
         for (int i = 0; i < packets; i++) {
             // Make 512 bytes of random data.
@@ -81,23 +85,40 @@ public class DatagramTester {
             packet.setData(receiveBuffer, 0, 512);
             try {
                 receiving_socket.receive(packet);
-                // Compare packet
+                // Calculate correctness, using for loop to compare sent and received data
                 double correctness = 0;
-                // Calculate correctness, using for loop to compare send and receive buffers
                 for (int j = 0; j < 512; j++) if (data[j] == receiveBuffer[j]) correctness++;
                 correctness = correctness / 512 * 100;
                 System.out.println("Packet "+i+" has a correctness of "+correctness+"%");
-                accuracy += correctness;
+                if (correctness >= 100) accuracy++;
+                if (burstlength > 0) {
+                    avgburstlength *= bursts-1;
+                    avgburstlength = (avgburstlength+burstlength)/bursts;
+                    burstlength = 0;
+                }
+                burstgap++;
             } catch (SocketTimeoutException e) {
                 loss++;
                 System.out.println("Packet "+i+" not received with a timeout of "+timeout+"ms");
+                if (burstgap > 0) {
+                    avgburstgap *= bursts;
+                    bursts++;
+                    avgburstgap = (avgburstgap+burstgap)/bursts;
+                    burstgap = 0;
+                }
+                burstlength++;
             }
         }
         // Calculate packet loss and accuracy
         System.out.println("\nSent "+packets+" packets and lost "+(int)loss);
-        double received = 100 - (loss / packets * 100);
-        System.out.println("Percentage of packets received: "+received+"%");
-        accuracy = accuracy / (packets - loss);
-        System.out.println("Accuracy of received packets: "+accuracy+"%");
+        accuracy = accuracy / packets * 100;
+        System.out.println("Percentage of correct packets: "+accuracy+"%");
+        loss = loss / packets * 100;
+        System.out.println("Percentage of packets lost: "+loss+"%");
+        if (bursts > 0) {
+            System.out.println("Number of bursts: "+bursts);
+            System.out.println("Average burst length: " + avgburstlength);
+            System.out.println("Average burst gap: " + avgburstgap);
+        }
     }
 }
